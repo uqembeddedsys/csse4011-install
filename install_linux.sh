deee6d05a01975d93ef8cfa5610e114c13dfa52e
MattDsouza <mattdsouza80@gmail.com>
2024-01-28 21:51:50 +1000

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -15,6 +15,7 @@ mkdir $HOME/zephyr_install
 wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.4/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz -P $HOME/zephyr_install
 tar xvf $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz -C $HOME/zephyr_install
 rm $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz
+$HOME/zephyr_install/zephyr-sdk-0.16.4/setup -t arm-zephyr-eabi -h -c
 
 #Allow access to serial ports
 sudo apt-get remove modemmanager brltty	#Remove serial terminal interference
