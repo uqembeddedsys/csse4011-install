019a69e32e5eaa97003d7214e2f6e85cd2fad733
MattDsouza <mattdsouza80@gmail.com>
2024-01-28 21:21:53 +1000

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -10,17 +10,21 @@ sudo apt install --no-install-recommends git cmake ninja-build gperf \
   python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
   make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1
 
-#install zephyr west tool
-sudo apt install -y python3-venv    #install python virtual environment
-pip install west
-
 #download and install zephyr sdk
 mkdir $HOME/zephyr_install
 wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.4/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz -P $HOME/zephyr_install
 tar xvf $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz -C $HOME/zephyr_install
 rm $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz
 
-
 #Allow access to serial ports
 sudo apt-get remove modemmanager brltty	#Remove serial terminal interference
 sudo adduser $USER dialout		#Add user permissions for dialout
+
+#install zephyr west tool
+sudo apt install -y python3-venv    #install python virtual environment
+cd $HOME/zephyr_install
+python -m venv env
+source $HOME/zephyr_install/env/bin/activate
+cd $HOME
+echo source $HOME/zephyr_install/env/bin/activate >> $HOME/.bashrc
+pip install west
