15cad47c4f5060b5c8f90db5aa8c1af8cb214cef
MattDsouza <mattdsouza80@gmail.com>
2024-01-28 20:06:01 +1000

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -17,7 +17,7 @@ pip install west
 #download and install zephyr sdk
 mkdir $HOME/zephyr_install
 wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.4/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz -P $HOME/zephyr_install
-tar xvf $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz
+tar xvf $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz -C $HOME/zephyr_install
 rm $HOME/zephyr_install/zephyr-sdk-0.16.4_linux-x86_64_minimal.tar.xz
 
 
