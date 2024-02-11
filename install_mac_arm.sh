a361f4edce866dd42e0cd1136539c6e07336479e
MattDsouza <mattdsouza80@gmail.com>
2024-02-12 01:38:05 +1000

    Update install_mac_arm.sh

--- install_mac_arm.sh
+++ install_mac_arm.sh
@@ -1,1 +1,26 @@
+# Mac OSX ARM CPU ONLY Installation for Zephyr RTOS
+# Based on zephyr install script
+
+brew install cmake ninja gperf python3 ccache qemu dtc libmagic
+
+#download and install zephyr sdk
+mkdir $HOME/zephyr_install
+wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.5/zephyr-sdk-0.16.5_macos-aarch64_minimal.tar.xz -P $HOME/zephyr_install
+tar xvf $HOME/zephyr_install/zephyr-sdk-0.16.5_macos-aarch64_minimal.tar.xz -C $HOME/zephyr_install
+rm $HOME/zephyr_install/zephyr-sdk-0.16.5_macos-aarch64_minimal.tar.xz
+$HOME/zephyr_install/zephyr-sdk-0.16.4/setup.sh -t arm-zephyr-eabi -h -c
+
+#install zephyr west tool
+cd $HOME/zephyr_install
+python3 -m venv env
+source $HOME/zephyr_install/env/bin/activate
+cd $HOME
+echo source $HOME/zephyr_install/env/bin/activate >> $HOME/.bashrc
+pip3 install west
+source $HOME/.bashrc
+
+#install nrf command line tools
+wget https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-0/nrf-command-line-tools-10.24.0-darwin.dmg -P $HOME/zephyr_install
+cd $HOME/zephyr_install
+hdiutil mount nrf-command-line-tools-10.24.0-darwin.dmg
 
