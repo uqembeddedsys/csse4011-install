6985269507176b8d287c09bdecb230e0f4d4f6bd
MattDsouza <mattdsouza80@gmail.com>
2024-02-12 01:41:11 +1000

    Create install_brew_intel.sh

---
+++ install_brew_intel.sh
@@ -0,0 +1,7 @@
+# Installation files for Brew
+# USE ONLY FOR Mac OSX ARM CPU Only
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
+source ~/.zprofile
