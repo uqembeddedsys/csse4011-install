a7f17a5072c0d9321d4cd015433d3e36fdd6251b
MattDsouza <mattdsouza80@gmail.com>
2024-02-12 23:12:29 +1000

    Update install_mac_intel.sh

--- install_mac_intel.sh
+++ install_mac_intel.sh
@@ -8,7 +8,7 @@ brew install cmake ninja gperf python3 ccache qemu dtc libmagic
 
 #download and install zephyr sdk
 mkdir $HOME/zephyr_install
-cd $HOME/zephyr_install && curl -O  https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.5/zephyr-sdk-0.16.5_macos-x86_64_minimal.tar.xz
+cd $HOME/zephyr_install && curl -L -O  https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.16.5/zephyr-sdk-0.16.5_macos-x86_64_minimal.tar.xz
 tar xvf $HOME/zephyr_install/zephyr-sdk-0.16.5_macos-x86_64_minimal.tar.xz -C $HOME/zephyr_install
 rm $HOME/zephyr_install/zephyr-sdk-0.16.5_macos-x86_64_minimal.tar.xz
 $HOME/zephyr_install/zephyr-sdk-0.16.4/setup.sh -t arm-zephyr-eabi -h -c
@@ -25,6 +25,6 @@ source $HOME/.zprofile
 source $HOME/.bashrc
 
 #install nrf command line tools
-cd $HOME/zephyr_install && curl -O https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-0/nrf-command-line-tools-10.24.0-darwin.dmg
+cd $HOME/zephyr_install && curl -L -O https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x/10-24-0/nrf-command-line-tools-10.24.0-darwin.dmg
 cd $HOME/zephyr_install
 hdiutil mount nrf-command-line-tools-10.24.0-darwin.dmg
